c0ab6c01337fdad570fd5fbbbb694a9ff1d2bb47
ChoiGyeongJu <rudwn5142@naver.com>
2024-11-04 15:30:17 +0900

    feat: footer 영역 hits 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,6 +6,9 @@ const Footer = () => {
       <FooterContent>
         <div>© 2024 Choi Gyeongju. All Rights Reserved.</div>
         <FooterLinks>
+          <a href="https://hits.seeyoufarm.com">
+            <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fchoigyeongju.github.io%2Fportfolio%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
+          </a>
           <a href="https://github.com/ChoiGyeongJu" target="_blank">
             GitHub
           </a>
